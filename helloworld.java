8ecc7f3224a2a2eaa567f7b322b5e60eb2765a25
hnwalters <31044184+hnwalters@users.noreply.github.com>
2017-08-15 14:09:41 -0500

    Add files via upload

---
+++ helloworld.java
@@ -0,0 +1,34 @@
+package hello;
+
+public class helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int variableName = 5; //this is our first variable!
+char character= 'c'; // a character variable goes in single quotes
+String hello = "Hello,world "; // a string variable goes in double quotes
+
+int x = 6; // be careful of integer division!
+int y = 10; // use "(double)" to typecast and get the correct answer!
+
+//System.out.println(1/0==5); //use "==" to check equality, and "!=" to check inequality
+//short-circuting can be useful!
+
+//determining the factorial of a number, n
+int n = 15;
+int answer = 1;
+for(int i=1; i<=n; i=i+1)
+{
+	answer = i*answer;
+}
+
+System.out.println(answer);
+//while(true) // a top tested (infinte) loop
+		
+//{
+//System.out.println(x);
+//x = x+1;
+
+	
+}}
+	
